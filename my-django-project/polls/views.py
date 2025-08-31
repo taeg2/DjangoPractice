3555e76c67da8bb8067a9775d2226d544d8aab4a
TaeGyoung Lee <90079870+taeg2@users.noreply.github.com>
2025-08-31 11:38:13 +0000

    part4 generic view 사용하기 까지 done

--- my-django-project/polls/views.py
+++ my-django-project/polls/views.py
@@ -2,21 +2,25 @@ from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "index.html"
+    context_object_name = "latest_question_list"
 
-def detail(request, question_id):
-    qusetion = get_object_or_404(Question, pk = question_id)
-    return render(request, "polls/detail.html", {"question":qusetion})
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
